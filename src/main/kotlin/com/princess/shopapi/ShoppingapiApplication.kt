2edb0201d0e658e2a2199368a488424d8a2eb17e
Princess Ethel Atillo <peatillo@vauldex.com>
2025-12-05 08:05:19 +0800

    Initial commit

---
+++ src/main/kotlin/com/princess/shopapi/ShoppingapiApplication.kt
@@ -0,0 +1,11 @@
+package com.princess.shopapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppingapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShoppingapiApplication>(*args)
+}
