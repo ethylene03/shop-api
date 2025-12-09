fa7ce4a4021370273411774053b1c46e638cfb7b
Princess Ethel <ethylcess101@gmail.com>
2025-12-09 13:07:33 +0800

    fix: create cart repository

--- src/main/kotlin/com/princess/shopapi/repository/CartRepository.kt
+++ src/main/kotlin/com/princess/shopapi/repository/CartRepository.kt
@@ -3,8 +3,10 @@ package com.princess.shopapi.repository
 import com.princess.shopapi.model.CartEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
 import java.util.UUID
 
 @Repository
 interface CartRepository : JpaRepository<CartEntity, UUID> {
+    fun findByUserId(userId: UUID): Optional<CartEntity>
 }
